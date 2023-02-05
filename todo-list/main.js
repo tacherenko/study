8896b5fe8713105aa7bf97e052da15d05bda0818
Tanya Cherenko <litrila@MacBook-Air-Tatana.local>
2023-02-05 21:07:08 +0300

    some project changes

--- todo-list/main.js
+++ todo-list/main.js
@@ -10,95 +10,97 @@ root.style.gap = "36px";
 root.style.width = "600px";
 root.style.height = "500px";
 root.style.backgroundColor = "#d5d5d5";
+
 // btn
-const deliteAll = document.createElement("button"); // создаем новый элемент <button>
-const textButton = document.createTextNode("Delete All"); // создаем текстовое содержимое
-deliteAll.appendChild(textButton); // добавляем текстовое содержимое элементу <button>
-root.appendChild(deliteAll);
-deliteAll.classList.add("deliteAll-button");
-// // input
-// const enterToDo = document.createElement("input");
-// enterToDo.classList.add("input-to-do");
-// root.appendChild(enterToDo);
-// enterToDo.placeholder = "Enter todo ...";
-// // btn two
-// const deliteAllTwo = document.createElement("button"); // создаем новый элемент <button>
-// const textButtonTwo = document.createTextNode("Add"); // создаем текстовое содержимое
-// deliteAllTwo.appendChild(textButtonTwo); // добавляем текстовое содержимое элементу <button>
-// root.appendChild(deliteAllTwo);
-// deliteAllTwo.classList.add("deliteAll-button");
-// // wrap (div)
-// const divWrap = document.createElement("div");
-// root.appendChild(divWrap);
-// divWrap.appendChild(deliteAll);
-// divWrap.appendChild(enterToDo);
-// divWrap.appendChild(deliteAllTwo);
-// divWrap.classList.add("wrap");
+const deleteAll = document.createElement("button"); // создаем новый элемент <button>
+const textButton = document.createTextNode("Delete All"); // создаем в нем текст
+deleteAll.appendChild(textButton); // добавляем этот текст элементу <button>
+root.appendChild(deleteAll); // присваиваем элемент <button> корневому элементу
+deleteAll.classList.add("deleteAll-btn"); //создаем класс для элемента <button>
 
-// // div innerBig
-// const divInnerBig = document.createElement("div");
-// root.appendChild(divInnerBig);
-// // divInnerBig.innerHTML = ''
-// divInnerBig.classList.add("innerBig");
+// input
+const inputToDo = document.createElement("input"); // создаем новый элемент <input>
+root.appendChild(inputToDo); //присваиваем элемент <input> корневому элементу
+inputToDo.classList.add("input-to-do"); // создаем новый элемент <input>
+inputToDo.placeholder = "Enter todo ..."; // пишем текст в плейсхолдер
 
-// // btnEnter
-// const enter = document.createElement(`button`);
-// divInnerBig.appendChild(enter);
-// const textButtonEnter = document.createTextNode("v");
-// enter.appendChild(textButtonEnter);
-// enter.classList.add("enterBnt");
+// btn 2
+const addBtn = document.createElement("button"); //создаем новый элемент <button>
+const textButtonTwo = document.createTextNode("Add"); // создаем текстовое содержимое
+addBtn.appendChild(textButtonTwo); // добавляем текстовое содержимое элементу <button>
+root.appendChild(addBtn); // присваиваем элемент <button> корневому элементу
+addBtn.classList.add("add-btn"); //создаем класс для элемента <button>
 
-// // input
-// const todoText = document.createElement("input");
-// divInnerBig.appendChild(todoText);
-// todoText.setAttribute(`type`, `text`);
-// todoText.classList.add("todoText");
-// todoText.placeholder = "Todo text";
+// wrap (div)
+const divWrap = document.createElement("div"); //создаем новый элемент <div>
+root.appendChild(divWrap); // присваиваем элемент <div> корневому элементу
+divWrap.appendChild(deleteAll); // присваиваем элемент <div> корневому элементу
+divWrap.appendChild(inputToDo); // присваиваем элемент <div> корневому элементу
+divWrap.appendChild(addBtn); // присваиваем элемент <div> корневому элементу
+divWrap.classList.add("wrap"); //создаем класс для элемента <div>
 
-// // btnClose inner
-// const closeBtn = document.createElement("button");
-// divInnerBig.appendChild(closeBtn);
-// const textCloseBtn = document.createTextNode("X");
-// closeBtn.appendChild(textCloseBtn);
-// closeBtn.classList.add("closeBtn");
+// div innerBig
+const divInnerBig = document.createElement("div"); //создаем новый элемент <div>
+root.appendChild(divInnerBig); // присваиваем элемент <div> корневому элементу
+divInnerBig.classList.add("innerBig"); //создаем класс для элемента <div>
 
-// // date
-// const dateInput = document.createElement("input");
-// divInnerBig.appendChild(dateInput);
-// dateInput.classList.add("date-input");
-// dateInput.setAttribute(`type`, `text`);
-// dateInput.placeholder = "Date";
+// btnEnter
+const enter = document.createElement("button"); //создаем новый элемент <button>
+divInnerBig.appendChild(enter); // присваиваем элемент <button> корневому элементу
+const textButtonEnter = document.createTextNode("v"); // добавляем текстовое содержимое элементу <button>
+enter.appendChild(textButtonEnter); // присваиваем элемент <button> корневому элементу
+enter.classList.add("enterBnt"); //создаем класс для элемента <button>
 
-// // div innerBig
-// const divInnerBig2 = document.createElement("div");
-// root.appendChild(divInnerBig2);
-// // divInnerBig.innerHTML = ''
-// divInnerBig2.classList.add("innerBig2");
+// input
+const todoText = document.createElement("input"); // создаем новый элемент <input>
+divInnerBig.appendChild(todoText); // присваиваем элемент <input> корневому элементу
+todoText.setAttribute(`type`, `text`); // присваиваем атрибуты <input>
+todoText.classList.add("todoText"); // создаем класс для элемента <input>
+todoText.placeholder = "Todo text"; // пишем текст в плейсхолдер
 
-// // btnEnter
-// const enter2 = document.createElement(`button`);
-// divInnerBig2.appendChild(enter2);
-// const textButtonEnter2 = document.createTextNode("v");
-// enter2.appendChild(textButtonEnter2);
-// enter2.classList.add("enterBnt");
+// btnClose inner
+const closeBtn = document.createElement("button"); //создаем новый элемент <button>
+divInnerBig.appendChild(closeBtn); // присваиваем элемент <button> корневому элементу
+const textCloseBtn = document.createTextNode("x"); // добавляем текстовое содержимое элементу <button>
+closeBtn.appendChild(textCloseBtn); // присваиваем элемент <button> корневому элементу
+closeBtn.classList.add("closeBtn"); //создаем класс для элемента <button>
 
-// // input
-// const todoText2 = document.createElement("input");
-// divInnerBig2.appendChild(todoText2);
-// todoText2.setAttribute(`type`, `text`);
-// todoText2.classList.add("todoText");
-// todoText2.placeholder = "Todo text";
+// date
+const dateInput = document.createElement("input"); //создаем новый элемент <input>
+divInnerBig.appendChild(dateInput); // присваиваем элемент <input> корневому элементу
+dateInput.setAttribute(`type`, `text`); // присваиваем атрибуты <input>
+dateInput.placeholder = "Date"; // пишем текст в плейсхолдер
+dateInput.classList.add("date-input"); //создаем класс для элемента <input>
 
-// // btnClose inner
-// const closeBtn2 = document.createElement("button");
-// divInnerBig2.appendChild(closeBtn2);
-// const textCloseBtn2 = document.createTextNode("X");
-// closeBtn2.appendChild(textCloseBtn2);
-// closeBtn2.classList.add("closeBtn");
+// div innerBig
+const divInnerBig2 = document.createElement("div"); //создаем новый элемент <div>
+root.appendChild(divInnerBig2); // присваиваем элемент <div> корневому элементу
+divInnerBig2.classList.add("innerBig2"); //создаем класс для элемента <div>
 
-// // date
-// const dateInput2 = document.createElement("input");
-// divInnerBig2.appendChild(dateInput2);
-// dateInput2.classList.add("date-input");
-// dateInput2.setAttribute(`type`, `text`);
-// dateInput2.placeholder = "Date";
+// btnEnter
+const enter2 = document.createElement("button"); //создаем новый элемент <button>
+divInnerBig2.appendChild(enter2); // присваиваем элемент <button> корневому элементу
+const textButtonEnter2 = document.createTextNode("v"); // добавляем текстовое содержимое элементу <button>
+enter2.appendChild(textButtonEnter2); // присваиваем элемент <button> корневому элементу
+enter2.classList.add("enterBnt"); // создаем класс для элемента <button>
+
+// input
+const todoText2 = document.createElement("input"); //создаем новый элемент <input>
+divInnerBig2.appendChild(todoText2); // присваиваем элемент <input> корневому элементу
+todoText2.setAttribute(`type`, `text`); // присваиваем атрибуты <input>
+todoText2.classList.add("todoText"); //создаем класс для элемента <input>
+todoText2.placeholder = "Todo text"; // пишем текст в плейсхолдер
+
+// btnClose inner
+const closeBtn2 = document.createElement("button"); //создаем новый элемент <button>
+divInnerBig2.appendChild(closeBtn2); // присваиваем элемент <button> корневому элементу
+const textCloseBtn2 = document.createTextNode("x"); // добавляем текстовое содержимое элементу <button>
+closeBtn2.appendChild(textCloseBtn2); // присваиваем элемент <button> корневому элементу
+closeBtn2.classList.add("closeBtn"); // создаем класс для элемента <button>
+
+// date
+const dateInput2 = document.createElement("input"); //создаем новый элемент <input>
+divInnerBig2.appendChild(dateInput2); // присваиваем элемент <input> корневому элементу
+dateInput2.classList.add("date-input"); //создаем класс для элемента <input>
+dateInput2.setAttribute(`type`, `text`); // присваиваем атрибуты <input>
+dateInput2.placeholder = "Date"; // пишем текст в плейсхолдер
